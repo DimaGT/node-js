9db3d5ac07baa52d68202e198cd03b71bbf3c0ac
DimaGT <dimon200117@gmail.com>
2022-09-12 20:47:42 +0300

    fix

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 const fs = require("fs");
 
-fs.readFile("./test.txt", 'utf-8', (error, data) => {
+fs.readFile("./test.txt", 'utf-8', (error, data) => { 
 
   fs.mkdirSync("./files", () => {});
-
   fs.writeFileSync("./files/test2.txt", `${data}New text!`, (error) => {
     error ? console.log(error) : null;
   });
