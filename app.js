af3a132b1bbdea93fe0874618d3b0cae685bac75
DimaGT <dimon200117@gmail.com>
2022-09-12 20:54:14 +0300

    fix

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const Logger = require('./log');
 const logger = new Logger();
 
 logger.on('some_event', (args) => {
-  const { id, text } = args;
-  console.log(id, text);
+  const { id, text } = args; 
+  console.log(id, text); 
 });
 
 logger.log('User Logged!');
